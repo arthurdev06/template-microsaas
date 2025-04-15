fba9d312c266cc92809b51daad712fcd75c53f2f
Arthur Rocha <arthurdev6@gmail.com>
2025-04-15 07:09:11 -0300

    add base directories

---
+++ app/(project)/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8 font-black">
+      Login Page
+    </div>
+  );
+}
