546ee55e72f593860ceff0eb8b09a8e488782fea
Arthur Rocha <arthurdev6@gmail.com>
2025-04-15 06:22:44 -0300

    first commit

---
+++ app/(project)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8 font-black">
+      Hello, World!
+    </div>
+  );
+}
